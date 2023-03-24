3ffdcb00695de35c7e78b23dfff98c72b04f9488
kianeblackman <kianeblackman@gmail.com>
2023-03-24 18:46:06 +0000

    react router installed to app and initial routes added to app file

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import './Styles/App.css';
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import logo from './Assets/svg/pizza-shark-logo.svg';
+import { Router, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Menu from './Pages/Menu/Menu';
+import BookATable from './Pages/BookATable/BookATable';
+import Gallery from './Pages/Gallery/Gallery';
+import Contact from './Pages/Contact/Contact';
 
 function App() {
   return (
@@ -10,6 +16,14 @@ function App() {
         <img src={logo} alt='Pizza Shark Logo' />
       </div>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/menu' element={<Menu />} />
+        <Route path='/book-a-table' element={<BookATable />} />
+        <Route path='/gallery' element={<Gallery />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
       <div className='green'></div>
       <div className='red'></div>
       <div className='blue'></div>
