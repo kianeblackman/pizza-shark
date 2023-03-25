55f1d2e3e34682d16ae65466f8a43f9909811da0
kianeblackman <kianeblackman@gmail.com>
2023-03-25 13:14:11 +0000

    react icons installed to app and added to footer component

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import {
+  AiFillPhone,
+  AiFillHome,
+  AiFillMail,
+  AiFillInstagram,
+} from 'react-icons/ai';
 
 export default function Footer() {
   return (
     <div className='footer'>
-      <div className='footer-text-container'>
-        <p>Footer</p>
+      <div className='marquee'>
+        <div className='contact-container'>
+          <AiFillPhone />
+          <h3>0845 054 3500</h3>
+        </div>
+        <div className='contact-container'>
+          <AiFillHome />
+          <h3>Hartlepool, Cleveland St TS24 7LH</h3>
+        </div>
+        <div className='contact-container'>
+          <AiFillMail />
+          <h3>pizzashark@gmail.com</h3>
+        </div>
+        <div className='contact-container'>
+          <AiFillInstagram />
+          <h3>@pizza_shark</h3>
+        </div>
       </div>
     </div>
   );
