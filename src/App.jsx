0ccfb7c9f00d63f6a401b78550b5ee44b0e5983d
kianeblackman <kianeblackman@gmail.com>
2023-03-25 12:49:02 +0000

    footer files added with base component structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@
 
 // Import Statments
 import React from 'react';
-import Navbar from './Components/Header/Navbar';
 import { Route, Routes } from 'react-router-dom';
 
 // Import Pages
@@ -15,6 +14,7 @@ import NotFound from './Pages/NotFound/NotFound';
 
 // Import Components
 import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 
 // Import Styles
 import './scss/styles.css';
@@ -31,6 +31,7 @@ function App() {
         <Route path='/contact' element={<Contact />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+      <div className='footer-text-container'>
+        <p>Footer</p>
+      </div>
+    </div>
+  );
+}
