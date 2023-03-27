709ffcc8a7f73413aee070cf43f594e7363a16d1
kianeblackman <kianeblackman@gmail.com>
2023-03-27 17:48:58 +0100

    adjustmments made to home page carosel

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import OrderHereButton from '../../Components/Buttons/OrderHereButton';
+import OrderHereButton from '../../components/Buttons/OrderHereButton';
 import image from '../../Assets/images/our-story.jpg';
 import logo from '../../Assets/svg/pizza-shark-logo.svg';
 
@@ -66,45 +66,16 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className='reviews-section'>
+      <section className='review-section'>
         <h2>Reviews</h2>
-        {/* <div className='reviews-content'>
-          <ReviewCard
-            rating='🍕🍕🍕🍕'
-            review='I had the pleasure of sinking my teeth into the Jawsome pizza at Pizza Shark and let me tell you, it was fin-tastic! The perfect combination of toppings and cheese. Highly recommend.'
-            author='Bruce S'
-          />
-          <ReviewCard
-            rating='🍕'
-            review='Im sorry to say that I didnt have a great experience at PizzaShark. The crust was a bit too chewy for my liking and the service was lacking. Maybe I just had bad luck with my visit.'
-            author='Debbie Downer'
-          />
-          <ReviewCard
-            rating='🍕🍕🍕🍕'
-            review='Pizza Shark is my go-to spot for a slice (or five) of pizza. The Sharknado pizza is my personal favorite - the perfect amount of heat and flavor. Cant recommend this place enough!'
-            author='Mike R'
-          />
-          <ReviewCard
-            rating='🍕🍕'
-            review='The pizza at Pizza Shark was mediocre at best. Ive had better frozen pizza from the grocery store. Definitely not worth the price.'
-            author='Karen K'
-          />
-          <ReviewCard
-            rating='🍕🍕'
-            review='The pizza at Pizza Shark was mediocre at best. Ive had better frozen pizza from the grocery store. Definitely not worth the price.'
-            author='Karen K'
-          />
-          <ReviewCard
-            rating='🍕🍕'
-            review='The pizza at Pizza Shark was mediocre at best. Ive had better frozen pizza from the grocery store. Definitely not worth the price.'
-            author='Karen K'
-          />
-        </div> */}
+        <h3>The good, the bad and the ugle.</h3>
         <ReviewCarousel />
       </section>
       <section className='footer-section'>
         <div className='footer-content'>
-          <img src={logo} alt='Pizza Shark Logo' />
+          <div className='logo'>
+            <img src={logo} alt='Pizza Shark Logo' />
+          </div>
           <p>
             1234 Shark Street, Brighton, UK
             <br />
--- src/Pages/Home/ReviewCarousel.jsx
+++ src/Pages/Home/ReviewCarousel.jsx
@@ -25,14 +25,19 @@ export default function ReviewCarousel() {
         author='Karen K'
       />
       <ReviewCard
-        rating='🍕🍕'
-        review='The pizza at Pizza Shark was mediocre at best. Ive had better frozen pizza from the grocery store. Definitely not worth the price.'
-        author='Karen K'
+        rating='🍕🍕🍕🍕🍕'
+        review='Pizza Shark is hands down the best pizza joint in Brighton! Im a regular here and every time I come in, Im blown away by the quality of the food and the friendly atmosphere.'
+        author='Jason B'
       />
       <ReviewCard
-        rating='🍕🍕'
-        review='The pizza at Pizza Shark was mediocre at best. Ive had better frozen pizza from the grocery store. Definitely not worth the price.'
-        author='Karen K'
+        rating='🍕🍕🍕🍕'
+        review='Im sorry to say that my visit to Pizza Shark was not a pleasant one. The pizza was burnt and the toppings were sparse. And the prices were pretty steep for what you got. I wouldnt recommend this place.'
+        author='Sarah L'
+      />
+      <ReviewCard
+        rating='🍕🍕🍕🍕'
+        review='As a vegetarian, Im always on the lookout for great pizza places with tasty veggie options, and Pizza Shark definitely delivers! Yum!'
+        author='Rachel G'
       />
     </div>
   );
