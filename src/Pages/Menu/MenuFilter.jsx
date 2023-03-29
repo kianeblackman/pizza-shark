2e85d4fb6d676c95543c1785cb03eb5e135a3fb1
kianeblackman <kianeblackman@gmail.com>
2023-03-29 10:20:00 +0100

    styling added to filter buttons on the menu filter bar

--- src/Pages/Menu/MenuFilter.jsx
+++ src/Pages/Menu/MenuFilter.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function MenuFilter() {
   return (
-    <div>
+    <div className='menu-filter-bar'>
       <h2>Menu</h2>
       <div className='filter-container'>
         <div className='refine-filter'>
