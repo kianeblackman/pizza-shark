0b7492596a6e8f9ef33de2a488ef58319d593bbe
kianeblackman <kianeblackman@gmail.com>
2023-03-29 09:55:53 +0100

    disclaimer text added to menu page

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
 export default function Menu() {
-  return <div>Menu</div>;
+  return (
+    <div>
+      <section className='disclaimer-section'>
+        <div className='disclaimer-content'>
+          <h2>Disclaimer</h2>
+          <p>
+            No actual sharks were harmed in the making of our pizza. Any
+            resemblance to a shark fin or any other part of a shark is purely
+            coincidental and delicious. Please do not attempt to swim with our
+            pizza, as it may cause severe cravings and hunger pains. We are not
+            responsible for any attempts to catch or ride a pizza shark. Thank
+            you for choosing Pizza Shark, where the pizza is always a bite above
+            the rest!
+          </p>
+        </div>
+      </section>
+    </div>
+  );
 }
