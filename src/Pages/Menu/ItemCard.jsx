fbc59095009c4fba6cc6ca108c612f3d909da6d4
kianeblackman <kianeblackman@gmail.com>
2023-04-03 10:56:07 +0100

    grid added to item card as well as pizza price and add to cart button

--- src/Pages/Menu/ItemCard.jsx
+++ src/Pages/Menu/ItemCard.jsx
@@ -3,18 +3,19 @@ import React from 'react';
 import itemArr from '../../data/itemArr';
 
 export default function ItemCard() {
-  const itemList = itemArr.map((itemArr) => (
-    <div className='item-card'>
+  const itemList = itemArr.map((itemArr, index) => (
+    <div className='item-card' key={index}>
       <div className='item-details'>
         <h3>{itemArr.name}</h3>
         <p>{itemArr.description}</p>
+        <h3>{itemArr.price}</h3>
         <h4>{itemArr.tag}</h4>
+      </div>
+      <div className='item-price'></div>
+      <div>
+        <button className='add-to-cart'>Add to Cart</button>
       </div>
     </div>
   ));
-  return (
-    <div className='item-card'>
-      <div>{itemList}</div>
-    </div>
-  );
+  return <div className='menu-grid'>{itemList}</div>;
 }
