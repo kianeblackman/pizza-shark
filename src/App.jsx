d4705eea88c3737fb1cac60f29e597e417a89a1f
kianeblackman <kianeblackman@gmail.com>
2023-04-03 14:15:17 +0100

    commented out home page

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ import { Route, Routes } from 'react-router-dom';
 // Import Pages
 import Home from './pages/home/Home';
 import Menu from './pages/menu/Menu';
-// import BookATable from './pages/bookATable/BookATable';
-// import Gallery from './pages/gallery/Gallery';
-// import Contact from './pages/contact/Contact';
-// import NotFound from './pages/notFound/NotFound';
+import BookATable from './pages/bookATable/BookATable';
+import Gallery from './pages/gallery/Gallery';
+import Contact from './pages/contact/Contact';
+import NotFound from './pages/notFound/NotFound';
 
 // Import Components
 import Header from './components/header/Header';
@@ -20,7 +20,7 @@ import OpenOrClosedSign from './components/openOrClosedSign/OpenOrClosedSign';
 // Import Styles
 import './scss/styles.css';
 
-function App() {
+export default function App() {
   return (
     <div>
       <Header />
@@ -29,13 +29,11 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/menu' element={<Menu />} />
-        {/* <Route path='/book-a-table' element={<BookATable />} />
+        <Route path='/book-a-table' element={<BookATable />} />
         <Route path='/gallery' element={<Gallery />} />
         <Route path='/contact' element={<Contact />} />
-        <Route path='*' element={<NotFound />} /> */}
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
   );
 }
-
-export default App;
