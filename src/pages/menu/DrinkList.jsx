58675909e7cc75db9fabf14f49c65bad9d678747
kianeblackman <kianeblackman@gmail.com>
2023-04-04 23:22:51 +0100

    temp drink svgs added to drinks lists

--- src/pages/menu/DrinkList.jsx
+++ src/pages/menu/DrinkList.jsx
@@ -6,10 +6,13 @@ export default function DrinkList() {
   const itemList = drinkArr.map((drinkArr, index) => (
     <div className='item-card' key={index}>
       <div className='item-details'>
-        <div className='item-main-details'>
-          <h3 className='item-name'>{drinkArr.name}</h3>
-          <h3 className='item-tag'>{drinkArr.type}</h3>
-          <p className='item-description'>{drinkArr.description}</p>
+        <div className='item-main-container'>
+          <div className='item-main-details'>
+            <h3 className='item-tag'>{drinkArr.type}</h3>
+            <img src={drinkArr.image} alt={drinkArr.alt} />
+            <h3 className='item-name'>{drinkArr.name}</h3>
+            <p className='item-description'>{drinkArr.description}</p>
+          </div>
         </div>
         <div className='item-sub-details'>
           <h3 className='item-price'>{drinkArr.price}</h3>
